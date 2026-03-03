573a796901c689a01f951332e0912a719fc861d2
Sujan S <sujanshivananda5@gmail.com>
2026-03-03 19:18:50 +0530

    fix : build error

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
+export async function GET(request: Request) {
     try {
         const cookieStore = await cookies();
         const token = cookieStore.get("token");
